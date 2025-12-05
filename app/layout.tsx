7079bc766cb43b81f0d2c7791e4872784c57bf0f
unpayedme <jv.sefuesca@gmail.com>
2025-12-05 13:43:38 +0800

    added logo and title

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,14 @@ import { AuthProvider } from '../src/contexts/AuthContext';
 import './globals.css';
 import { ReactNode } from 'react';
 
+
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <title>Locafy</title>
+        <link rel="icon" href="logo.jpg" />
+      </head>
       <body>
         <AuthProvider>
           {children}
