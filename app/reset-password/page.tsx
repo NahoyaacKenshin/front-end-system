5d74d29fbe9cd9862ef8ed52f0fc3da7acfe6f43
NahoyaacKenshin <kenjicaayohan55@gmail.com>
2025-12-05 12:22:07 +0800

    fix icon-spacing in reset-pass

--- app/reset-password/page.tsx
+++ app/reset-password/page.tsx
@@ -139,7 +139,7 @@ function ResetPasswordContent() {
               <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/>
             </svg>
           </button>
-          <h2 className="text-4xl mb-2.5 text-[#1a1a1a] text-center">RESET PASSWORD</h2>
+          <h2 className="text-4xl mb-2.5 mt-10 text-[#1a1a1a] text-center">RESET PASSWORD</h2>
           <p className="text-center text-gray-600 mb-10 text-[0.95rem]">Enter your new password</p>
           
           {error && (
